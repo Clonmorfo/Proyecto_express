1f2eceb5f21da895862870905b959554b82a57d0
Juan Salguero <salguerojuanfrancisco@gmail.com>
2022-07-19 20:18:04 -0300

    Cambio de lugar algunas carpetas

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
